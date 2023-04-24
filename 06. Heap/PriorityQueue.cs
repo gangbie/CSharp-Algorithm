e331734dbb2a545f3d5dad5a5c6cb4c196a6ce43
경빈 박 <rudqls1123@naver.com>
2023-04-24 20:07:18 +0900

    feat : heap

---
+++ 06. Heap/PriorityQueue.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{                              //실제 데이터, 우선순위
+    internal class PriorityQueue<TElement>   //TPriority 대신 무조건 int 써도 ㄱㅊ
+    {
+        private struct Node
+        {
+            public TElement element;
+            public int priority;
+        }
+
+        private List<Node> nodes;
+
+        public PriorityQueue()
+        {
+            this.nodes = new List<Node>();
+        }
+
+        public int Count { get { return nodes.Count; } }
+
+        public void Enqueue(TElement element, int priority)
+        {
+            Node newNode = new Node() { element = element, priority = priority };
+            // 1. 가장 뒤에 데이터 추가
+            nodes.Add(newNode);
+            int newNodeIndex = nodes.Count - 1;
+            // 2. 새로운 노드를 힙상태가 유지되도록 승격 작업 반복
+            while(newNodeIndex > 0)
+            {
+                // 2-1. 부모노드 확인
+                int parentIndex = GetParentIndex(newNodeIndex);
+                Node parentNode = nodes[parentIndex];
+
+                // 2-2. 자식노드가 부모노드보다 우선순위 높으면 교체
+                if(newNode.priority < parentNode.priority)
+                {
+                    nodes[newNodeIndex] = parentNode;
+                    nodes[parentIndex] = newNode;
+                    newNodeIndex = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public TElement Dequeue()
+        {
+            Node rootNode = nodes[0];
+
+            // 1. 0번 빼고서 마지막놈 최상단으로 올림(복사)
+            Node lastNode = nodes[nodes.Count - 1];
+            nodes[0] = lastNode;
+            // 1-1. 올렸으면 가장 마지막놈 지움
+            nodes.RemoveAt(nodes.Count - 1);
+
+            int index = 0;
+            // 2. 자식노드들과 비교하여 더 작은 자식과 교체
+            while(index < nodes.Count)
+            {
+                int leftChildIndex = GetLeftChildIndex(index);
+                int righttChildIndex = GetRightChildIndex(index);
+
+                // 2-1. 자식이 둘 다 있는 경우
+                if (righttChildIndex < nodes.Count)
+                {
+                    // 자식들 먼저 비교 lessChildIndex
+                    int lessChildIndex = nodes[leftChildIndex].priority < nodes[righttChildIndex].priority
+                        ? leftChildIndex : righttChildIndex;
+                    // less랑 현재랑 비교
+                    if (nodes[lessChildIndex].priority < nodes[index].priority)
+                    {
+                        nodes[index] = nodes[lessChildIndex];
+                        nodes[lessChildIndex] = lastNode;
+                        index = lessChildIndex;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                // 2-2. 자식이 하나만 있는 경우 == 왼쪽 자식만 있는 경우
+                else if (leftChildIndex < nodes.Count)
+                {
+                    if (nodes[leftChildIndex].priority < nodes[index].priority)
+                    {
+                        nodes[index] = nodes[leftChildIndex];
+                        nodes[leftChildIndex] = lastNode;
+                        index = leftChildIndex;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                // 2-3. 자식이 없는 경우
+                else
+                {
+                    break;
+                }
+            }
+
+            return rootNode.element;
+        }
+
+        public TElement Peak()
+        {
+            return nodes[0].element;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return parentIndex * 2 + 1;
+        }
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return parentIndex * 2 + 2;
+        }
+        public int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+    }
+}
---
+++ 06. Heap/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _06._Heap
+{
+    internal class Program
+    {
+
+        /******************************************************
+		 * 힙 (Heap)
+		 * 
+		 * 부모 노드가 항상 자식노드보다 우선순위가 높은 속성을 만족하는 트리기반의 자료구조
+		 * 많은 자료 중 우선순위가 가장 높은 요소를 빠르게 가져오기 위해 사용
+		 ******************************************************/
+        static void PriorityQueue()
+        {
+            // 기본 int 우선순위(오름차순) 적용
+            PriorityQueue<string, int> ascendingpq = new PriorityQueue<string, int>();
+
+            ascendingpq.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+            ascendingpq.Enqueue("데이터2", 3);
+            ascendingpq.Enqueue("데이터3", 5);
+            ascendingpq.Enqueue("데이터4", 2);
+            ascendingpq.Enqueue("데이터5", 4);
+
+            while (ascendingpq.Count > 0) Console.WriteLine(ascendingpq.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+
+
+            // 수정 int 우선순위 적용(내림차순)
+            PriorityQueue<string, int> decendingpq = new PriorityQueue<string, int>(Comparer<int>.Create((a, b) => b - a));
+
+            decendingpq.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+            decendingpq.Enqueue("데이터2", 3);
+            decendingpq.Enqueue("데이터3", 5);
+            decendingpq.Enqueue("데이터4", 2);
+            decendingpq.Enqueue("데이터5", 4);
+
+            while (decendingpq.Count > 0) Console.WriteLine(decendingpq.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+        }
+
+        // 시간복잡도
+        // 탐색(가장 우선순위 높은)     추가         삭제
+        // O(1)                    O(logN)      O(logN)
+
+        static void Main(string[] args)
+        {
+            // 기본 int 우선순위(오름차순) 적용
+            DataStructure.PriorityQueue<string> pq1 = new DataStructure.PriorityQueue<string>();
+            
+            pq1.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+            pq1.Enqueue("데이터2", 3);
+            pq1.Enqueue("데이터3", 5);
+            pq1.Enqueue("데이터4", 2);
+            pq1.Enqueue("데이터5", 4);
+            
+            while (pq1.Count > 0) Console.WriteLine(pq1.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+            
+            
+            // 수정 int 우선순위 적용
+            //DataStructure.PriorityQueue<string> pq2 = new DataStructure.PriorityQueue<string>(Comparer<int>.Create((a, b) => b - a));
+            //
+            //pq2.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+            //pq2.Enqueue("데이터2", 3);
+            //pq2.Enqueue("데이터3", 5);
+            //pq2.Enqueue("데이터4", 2);
+            //pq2.Enqueue("데이터5", 4);
+            //
+            //while (pq2.Count > 0) Console.WriteLine(pq2.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+        }
+    }
+}
