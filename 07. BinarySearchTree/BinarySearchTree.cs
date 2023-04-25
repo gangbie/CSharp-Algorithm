9e78e048be1d4c6012650b1bbe06430b85a166f0
경빈 박 <rudqls1123@naver.com>
2023-04-25 19:03:44 +0900

    feat : BinarySearchTree

---
+++ 07. BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private Node root;
+        public class Node
+        {
+            public T item;
+            public Node parent;
+            public Node left;
+            public Node right;
+            public Node(T item, Node parent, Node left, Node right)
+            {
+                this.item = item;
+                this.parent = parent;
+                this.left = left;
+                this.right = right;
+            }
+
+            public bool IsRootNode { get { return parent == null; } }
+            public bool IsLeftChild { get { return parent != null && parent.left == this; } }
+            public bool IsRightChild { get { return parent != null && parent.right == this; } }
+            public bool HasNoChild { get { return left == null && right == null; } }
+            public bool HasLeftChild { get { return left != null && right == null; } }
+            public bool HasRightChild { get { return left == null && right != null; } }
+            public bool HasBothChild { get { return left != null && right != null; } }
+        }
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+
+        public void Add(T item)
+        {
+            Node newNode = new Node(item, null, null, null);
+
+            if(root == null)
+            {
+                root = newNode;
+                return;
+            }
+            Node current = root;
+            while(current != null)
+            {
+                // 비교해서 더 작은 경우 왼쪽으로
+                if (item.CompareTo(current.item) < 0)
+                {
+                    // 비교노드가 왼쪽 자식이 있는 경우
+                    if(current.left != null)
+                    {
+                        // 왼쪽 자식과 또 비교하기 위해 current를 왼쪽 자식으로 설정
+                        current = current.left;
+                    }
+                    // 비교노드가 왼쪽 자식이 없는경우
+                    else
+                    {
+                        // 그자리가 지금 추가가 될 자리
+                        current.left = newNode;
+                        newNode.parent = current;
+                        return;
+                    }
+                }
+                // 더 큰경우 오른쪽으로
+                else if(item.CompareTo(current.item) > 0)
+                {
+                    // 비교노드가 오른쪽 자식이 있는 경우
+                    if(current.right != null)
+                    {
+                        // 오른쪽 자식과 또 비교하기 위해 current를 오른쪽자식으로 설정
+                        current = current.right;
+                    }
+                    // 비교노드가 오른쪽 자식이 없는 경우
+                    else
+                    {
+                        // 그 자리가 지금 추가가 될 자리
+                        current.right = newNode;
+                        newNode.parent = current;
+                        return;
+                    }
+                }
+                // 똑같은 경우
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        public bool TryGetValue(T item, out T outValue)
+        {
+            if(root == null)
+            {
+                outValue = default(T);
+                return false;
+            }
+
+            Node findNode = FindNode(item);
+            if (findNode == null)
+            {
+                outValue = default(T);
+                return false;
+            }
+            else
+            {
+                outValue = findNode.item;
+                return true;
+            }
+        }
+
+        public Node FindNode(T item)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            Node current = root;
+            while (current != null)
+            {
+                if (item.CompareTo(current.item) < 0)
+                {
+                    current = current.left;
+                }
+                else if (item.CompareTo(current.item) > 0)
+                {
+                    current = current.right;
+                }
+                else
+                {
+                    return current;
+                }
+            }
+            return null;
+        }
+
+        public bool Remove(T item)
+        {
+            Node findNode = FindNode(item);
+
+            if(findNode == null)
+            {
+                return false;
+            }
+            else
+            {
+                EraseNode(findNode);
+                return true;
+            }
+        }
+
+        private void EraseNode(Node node)
+        {
+            // 1. 자식노드가 없는 노드일 경우
+            if (node.HasNoChild)
+            {
+                if (node.IsLeftChild)
+                {
+                    node.parent.left = null;
+                }else if (node.IsRightChild)
+                {
+                    node.parent.right = null;
+                }
+                else
+                {
+                    root = null;
+                }
+            }
+            // 2. 자식노드가 1개인 노드일 경우
+            else if(node.HasLeftChild || node.HasRightChild)
+            {
+                Node parent = node.parent;
+                Node child = node.HasLeftChild ? node.left : node.right;
+                if (node.IsLeftChild)
+                {
+                    parent.left = child;
+                    child.parent = parent;
+                }
+                else if (node.IsRightChild)
+                {
+                    parent.right = child;
+                    child.parent = parent;
+                }
+                else // if(node.IsRootNode)
+                {
+                    root = child;
+                    child.parent = null;
+                }
+            }
+            // 3. 자식노드가 2개인 노드일 경우
+            // 왼쪽 자식 중 가장 큰 값과 데이터 교환한 후, 그 노드를 지워주는 방식으로 대체
+            // 왼쪽으로 한 칸 간 뒤 계속 오른쪽으로만 가면 됨
+            else // if(node.HasBothChild)
+            {
+                Node replaceNode = node.left;
+                while(replaceNode.right != null)
+                {
+                    replaceNode = replaceNode.right;
+                }
+                node.item = replaceNode.item;
+                EraseNode(replaceNode);
+
+                // 오른쪽꺼 제일 작은 값으로 해도 됨
+                /*
+                Node replaceNode = node.right;
+                while (replaceNode.left != null)
+                {
+                    replaceNode = replaceNode.left;
+                }
+                node.item = replaceNode.item;
+                EraseNode(replaceNode);
+                */
+            }
+        }
+        
+    }
+}
---
+++ 07. BinarySearchTree/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _07._BinarySearchTree
+{
+    internal class Program
+    {
+        /******************************************************
+		 * 트리 (Tree)
+		 * 
+		 * 계층적인 자료를 나타내는데 자주 사용되는 자료구조
+		 * 부모노드가 0개 이상의 자식노드들을 가질 수 있음
+		 * 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가질 수 없음
+		 ******************************************************/
+
+        /******************************************************
+		 * 이진탐색트리 (BinarySearchTree)
+		 * 
+		 * 이진속성과 탐색속성을 적용한 트리
+		 * 이진탐색을 통한 탐색영역을 절반으로 줄여가며 탐색 가능
+		 * 이진 : 부모노드는 최대 2개의 자식노드들을 가질 수 있음
+		 * 탐색 : 자신의 노드보다 작은 값들은 왼쪽, 큰 값들은 오른쪽에 위치
+		 ******************************************************/
+
+        // <이진탐색트리의 시간복잡도>
+        // 접근			탐색			삽입			삭제
+        // O(log n)		O(log n)	O(log n)	O(log n)
+
+        // <이진탐색트리의 주의점>
+        // 이진탐색트리의 노드들이 한쪽 자식으로만 추가되는 불균형 발생 가능
+        // 이 경우 탐색영역이 절반으로 줄여지지 않기 때문에 시간복잡도 증가
+        // 이러한 현상을 막기 위해 자가균형기능을 추가한 트리의 사용이 일반적
+        // 대표적인 방식으로 Red-Black Tree, AVL Tree 등이 있음
+
+        void BinarySearchTree()
+        {
+            // 키 == 데이터
+            SortedSet<int> sortedSet = new SortedSet<int>();
+
+            // 추가
+            sortedSet.Add(1);
+            sortedSet.Add(2);
+            sortedSet.Add(3);
+            sortedSet.Add(4);
+            sortedSet.Add(5);
+
+            // 탐색
+            int searchValue1;
+            bool find = sortedSet.TryGetValue(3, out searchValue1);
+
+            // 삭제
+            sortedSet.Remove(3);
+
+            // 탐색용 키, 실제 데이터    아래 int부분 꼭 int일 필요 없음 string도 정렬 가능
+            SortedDictionary<string, Monster> sortedDic = new SortedDictionary<string, Monster>();
+
+            sortedDic.Add("피카츄", new Monster() { name = "피카츄", hp = 100 });
+            sortedDic.Add("파이리", new Monster() { name = "파이리", hp = 120 });
+
+            Monster monster;
+            sortedDic.TryGetValue("파이리", out monster);  // 파이리 탐색 시도
+            Monster indexerMonster = sortedDic["파이리"];  // 인덱서를 통한 탐색
+
+            sortedDic.Remove("피카츄");
+        }
+        class Monster
+        {
+            public string name;
+            public int hp;
+            public int mp;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+}
